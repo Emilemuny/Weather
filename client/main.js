f1499077728b31bff00997732028baac202c3265
emilemuny <emilemuny007@gmail.com>
2015-02-11 16:37:01 -0800

    adding the interface

--- client/main.js
+++ client/main.js
@@ -1,1 +1,40 @@
-console.log('hello, world');
+'use strict';
+
+$(document).ready(init);
+
+function init() {
+  $('#get-cams').click(clickGetCams);
+  $('#nsa').click(clickNsa);
+
+}
+
+function clickNsa() {
+  var options = {enableHighAccuracy: true, timeout: 5000, maximumAge: 0};
+  nagivator.geolocation.getCurrentPosition(success, error, options);
+}
+
+function success(pos) {
+  var url = 'http://api.wunderground.com/api/a26aaa5dfad38572/webcams/q/' + pos.coords.latitude + ',' + pos.coords.longitude + '.json';   //put the geolocation json url here
+  paint(url);
+}
+
+function error(err) {
+  console.log('could not find position', err);
+}
+
+function clickGetCams() {
+  var zipCode = $('#zip-code').val();
+  var url = 'http://api.wunderground.com/api/a26aaa5dfad38572/webcams/q' + zipcode + '.json'; //put the cams json url here
+  paint(url);
+}
+
+function paint(url) {
+  $.getJSON(url, function(response){
+    response.webcams.forEach(function(cam){
+      var $img = $('<div>');
+      $img.addClass('img');
+      $img.css('background-image', 'url("  ' + cam.CURRENTIMAGEURL + ' ")')
+      $('#images').append($img);
+    });
+  });
+}
